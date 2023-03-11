80fcae465b57cd197b3a818e7d41618fbc6f9d26
MLoth <MLoth@users.noreply.github.com>
2023-03-11 06:04:06 +0100

    feat: more WIP

--- components/ContentLink.vue
+++ components/ContentLink.vue
@@ -9,9 +9,9 @@
       v-if="item.cover"
       :src="`${item._path}/${item.cover}`"
       format="webp"
-      :quality="80"
-      width="548"
-      height="365"
+      :width="548"
+      :height="365"
+      :fit="`cover`"
       class="w-full rounded-tl-lg rounded-tr-lg"
       loading="lazy"
       placeholder
