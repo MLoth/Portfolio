26bc087f6d5e4af53456f4a1399df87e0a491a5f
MLoth <MLoth@users.noreply.github.com>
2023-04-19 15:02:45 +0200

    fix: added img again

--- components/content/BlogImage.vue
+++ components/content/BlogImage.vue
@@ -5,9 +5,16 @@
     } mb-12 items-baseline gap-6 ${images.length === 1 ? '-mx-6 sm:mx-0' : ''}`"
   >
     <div v-for="{ url, alt, caption } in images">
-      < :src="`blog/${url}`" :alt="alt ? alt : caption" :caption="caption"
-      :imgAttrs="{ class: `w-full ${ images.length === 1 ? 'rounded-none
-      sm:rounded-md' : 'rounded-md' }`, }" loading="lazy" :width="getWidth()" />
+      <nuxt-img
+        :src="`blog/${url}`"
+        :alt="alt ? alt : caption"
+        :caption="caption"
+        :class="`w-full ${
+          images.length === 1 ? 'rounded-none sm:rounded-md' : 'rounded-md'
+        }`"
+        loading="lazy"
+        :width="getWidth()"
+      />
       <p
         v-if="caption"
         :class="`py-3 text-sm text-neutral-400 ${
