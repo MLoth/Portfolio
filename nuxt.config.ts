ecc74dc3a809a2273472cbbe2f96a2302903a39d
MLoth <MLoth@users.noreply.github.com>
2023-04-26 15:16:12 +0200

    feat: added selection colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'selection:bg-black selection:text-white',
+      },
+    },
+  },
+
   css: ['~/assets/fonts/geomanist.css'],
 
   modules: ['@unocss/nuxt', '@nuxt/content', '@nuxt/image-edge'],
@@ -11,7 +19,10 @@ export default defineNuxtConfig({
   unocss: {
     rules: [['font-theme', { 'font-family': 'Geomanist, sans-serif' }]],
     extraContent: { filesystem: ['./content/**/*.md'] },
-    safelist: [...Array.from({ length: 4 }, (_, i) => `md:grid-cols-${i + 1}`)],
+    safelist: [
+      ...Array.from({ length: 4 }, (_, i) => `md:grid-cols-${i + 1}`),
+      'selection:bg-black selection:text-white',
+    ],
   },
 
   // Nuxt Image
