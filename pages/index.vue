78e1729071e276975865817e144e46889cbd7e14
MLoth <MLoth@users.noreply.github.com>
2023-05-02 06:49:58 +0200

    feat: more professional related intro copy

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <GenericContainer>
     <HeroText front="Fullstack developer" back="Photographer" />
     <p class="-mt-20 mb-24 text-center opacity-30">
-      Also teacher, fervent walker & hobby photographer.
+      Also a teacher, who loves design & photography.
     </p>
 
     <div class="grid gap-6 align-baseline md:grid-cols-12 md:gap-12">
@@ -10,7 +10,9 @@
         class="pointer-events-none z-10 text-center leading-none tracking-widest md:col-span-12 md:mt-12"
       >
         <!-- <p class="-mt-20 mb-24 text-center opacity-30"></p> -->
-        <h2 class="text-3xl opacity-30">&lt;Projects&sol;&gt;</h2>
+        <h2 class="font-theme text-4xl font-semibold tracking-wide">
+          &lt;Projects&sol;&gt;
+        </h2>
       </div>
 
       <!-- PROJECT ITEMS -->
@@ -27,7 +29,7 @@
       <div class="my-12 flex justify-center md:col-span-12">
         <RouterLink
           to="/projects"
-          class="@dark:hover:bg-neutral-800 rounded-lg py-2 px-4 text-center text-sm font-bold hover:bg-neutral-100"
+          class="@dark:hover:bg-neutral-800 rounded-lg py-2 px-4 text-center text-sm font-bold opacity-20 hover:bg-neutral-100 hover:opacity-100"
         >
           All projects >
         </RouterLink>
@@ -37,7 +39,7 @@
         class="pointer-events-none z-10 text-center leading-none tracking-widest md:col-span-12 md:mt-12"
       >
         <!-- <p class="-mt-20 mb-24 text-center opacity-30"></p> -->
-        <h2 class="text-3xl opacity-30">Blog</h2>
+        <h2 class="font-theme text-4xl font-semibold tracking-wide">Blog</h2>
       </div>
 
       <!-- BLOG ITEMS -->
@@ -54,7 +56,7 @@
       <div class="my-12 flex justify-center md:col-span-12">
         <RouterLink
           to="/blog"
-          class="@dark:hover:bg-neutral-800 rounded-lg py-2 px-4 text-center text-sm font-bold hover:bg-neutral-100"
+          class="@dark:hover:bg-neutral-800 rounded-lg py-2 px-4 text-center text-sm font-bold opacity-20 hover:bg-neutral-100 hover:opacity-100"
         >
           All blog posts >
         </RouterLink>
