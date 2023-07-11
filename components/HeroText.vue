ebe1e454ba00283ab0d4416883c887bedaa0d98e
MLoth <MLoth@users.noreply.github.com>
2023-07-11 08:04:18 +0200

    style: removed logging

--- components/HeroText.vue
+++ components/HeroText.vue
@@ -3,11 +3,6 @@
     <div
       class="font-theme col-span-12 grid items-baseline pt-6 pb-24 text-center leading-none"
     >
-      <!-- <p
-        class="@dark:text-neutral-800 col-start-1 row-start-1 text-center font-mono text-5xl font-bold text-neutral-100 md:text-7xl"
-      >
-        {{ back }}
-      </p> -->
       <h1
         class="@dark:text-neutral-200 relative col-start-1 row-start-1 text-5xl font-bold tracking-wide md:text-7xl"
       >
