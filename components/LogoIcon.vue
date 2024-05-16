49530b91de5579445a590a7d2658cfbf6589f91d
Marty <MLoth@users.noreply.github.com>
2024-05-16 09:45:07 +0200

    fix: added logo animation support for safari

--- components/LogoIcon.vue
+++ components/LogoIcon.vue
@@ -5,36 +5,31 @@
     viewBox="0 0 852 207.75"
     :style="`stroke-linecap:round;stroke-linejoin:round;`"
   >
-    <!-- vector-effect="non-scaling-stroke" -->
     <path
-      ref="path"
+      class="text-neutral-200"
       d="m786.38,38.25c29.44,29.44,29.52,77.14.19,106.69h0c-29.48,29.69-77.48,29.77-107.06.19l-83.25-82.75c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69l-107.63-107.12c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69l-107.62-107.12c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69"
     />
     <path
+      ref="path"
       d="m786.38,38.25c29.44,29.44,29.52,77.14.19,106.69h0c-29.48,29.69-77.48,29.77-107.06.19l-83.25-82.75c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69l-107.63-107.12c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69l-107.62-107.12c-29.61-29.47-77.5-29.33-106.94.31l-.12.13c-29.34,29.54-29.25,77.25.19,106.69"
     />
   </svg>
 </template>
 
-<script lang="ts" setup>
-const pathLength = ref()
-const path = ref<SVGPathElement | null>(null)
-
-onMounted(() => {
-  if (path.value) pathLength.value = path.value.getTotalLength()
-})
-</script>
-
 <style scoped>
 .logo-animated {
-  stroke-dasharray: 1370.8946533203125;
-  stroke-dashoffset: -1370.8946533203125;
   animation: dash 1s cubic-bezier(0.68, -0.55, 0.265, 1.2) forwards;
+  stroke-dasharray: 1370.8946533203125;
+  stroke-dashoffset: 1370.8946533203125;
 }
 
 @keyframes dash {
+  from {
+    stroke-dashoffset: 1370.8946533203125;
+  }
+
   to {
-    stroke-dashoffset: 0;
+    stroke-dashoffset: 2741.789306640625;
   }
 }
 </style>
