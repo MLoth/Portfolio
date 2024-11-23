9eab7e9916e8ba57eedfee1b2066c77e4aa18a4f
Marty <MLoth@users.noreply.github.com>
2024-11-23 12:20:15 +0100

    feat: played around with bg blur

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="mx-6 py-6 sm:ml-6 sm:mr-6 sm:py-12">
-    <header
-      class="align-start mx-auto flex max-w-screen-2xl flex-col justify-start md:flex-row md:items-center md:justify-between"
-    >
-      <div class="flex justify-between">
-        <NuxtLink
-          to="/"
-          class="logo @dark:text-white @dark:hover:bg-neutral-800 z-30 -ml-3 flex items-center rounded-full p-3 text-neutral-900 ring-neutral-800 hover:bg-neutral-50 focus-visible:outline-none focus-visible:ring"
-        >
-          <h1>
-            <LogoIcon class="h-7" />
-          </h1>
-        </NuxtLink>
-
-        <MenuTrigger
-          :menu-available="menuAvailable"
-          @toggle-menu="toggleMenu"
-        />
-      </div>
-
-      <!-- To keep track of the fullscreen, easy to cut the growing circle -->
-      <div
-        :class="`${
-          showContainer && menuAvailable
-            ? 'clip-0 absolute -ml-6 -mt-12 h-screen w-screen overflow-hidden'
-            : ''
-        }`"
+  <div class="z-100 sticky top-0 bg-white/85 py-6 backdrop-blur-lg">
+    <div class="px-6">
+      <header
+        class="align-start mx-auto flex max-w-screen-2xl flex-col justify-start md:flex-row md:items-center md:justify-between"
       >
-        <div
-          :class="{
-            'max-h-0 overflow-hidden opacity-0':
-              menuAvailable && !showContainer,
-            '@dark:bg-neutral-300 clip-0 pt-42 absolute h-screen max-h-screen w-screen bg-white opacity-100':
-              menuAvailable && showContainer,
-            'opacity-100': !menuAvailable,
-          }"
-          class="z-20 flex md:max-h-none md:w-auto md:items-center"
-        >
-          <MenuContent>
-            <MenuItem link="/" text="Home" />
-            <MenuItem link="/blog" text="Blog" :blog-active="blogActive" />
-            <MenuItem link="/projects" text="Projects" />
-            <MenuItem link="/about" text="About me" />
-          </MenuContent>
+        <div class="flex justify-between">
+          <NuxtLink
+            to="/"
+            class="logo @dark:text-white @dark:hover:bg-neutral-800 z-30 -ml-3 flex items-center rounded-full p-3 text-neutral-900 ring-neutral-800 hover:bg-neutral-50 focus-visible:outline-none focus-visible:ring"
+          >
+            <h1>
+              <LogoIcon class="h-7" />
+            </h1>
+          </NuxtLink>
+
+          <MenuTrigger
+            :menu-available="menuAvailable"
+            @toggle-menu="toggleMenu"
+          />
         </div>
 
+        <!-- To keep track of the fullscreen, easy to cut the growing circle -->
         <div
-          :style="[
-            open && menuAvailable
-              ? { transform: `scale(${scale})` }
-              : { transform: `scale(0)` },
-          ]"
-          :class="`${open && menuAvailable ? `` : 'scale-0'}`"
-          class="@dark:bg-white transition-scale duration-400 pointer-events-none absolute right-3 top-9 z-50 h-24 w-24 overflow-hidden rounded-full bg-white mix-blend-difference ease-in-out"
-          @transitionend="circleShrank"
-        />
-      </div>
-    </header>
+          :class="`${
+            showContainer && menuAvailable
+              ? 'clip-0 absolute -ml-6 -mt-12 h-screen w-screen overflow-hidden'
+              : ''
+          }`"
+        >
+          <div
+            :class="{
+              'max-h-0 overflow-hidden opacity-0':
+                menuAvailable && !showContainer,
+              '@dark:bg-neutral-300 clip-0 pt-42 absolute h-screen max-h-screen w-screen bg-white opacity-100':
+                menuAvailable && showContainer,
+              'opacity-100': !menuAvailable,
+            }"
+            class="z-20 flex md:max-h-none md:w-auto md:items-center"
+          >
+            <MenuContent>
+              <MenuItem link="/" text="Home" />
+              <MenuItem link="/blog" text="Blog" :blog-active="blogActive" />
+              <MenuItem link="/projects" text="Projects" />
+              <MenuItem link="/about" text="About me" />
+            </MenuContent>
+          </div>
+
+          <div
+            :style="[
+              open && menuAvailable
+                ? { transform: `scale(${scale})` }
+                : { transform: `scale(0)` },
+            ]"
+            :class="`${open && menuAvailable ? `` : 'scale-0'}`"
+            class="@dark:bg-white transition-scale duration-400 pointer-events-none absolute right-3 top-9 z-50 h-24 w-24 overflow-hidden rounded-full bg-white mix-blend-difference ease-in-out"
+            @transitionend="circleShrank"
+          />
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
