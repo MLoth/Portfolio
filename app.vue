735f365a116b00a52e196325f7509fd4df0deeb8
Marty <MLoth@users.noreply.github.com>
2025-01-22 17:11:04 +0100

    migrated to setup mode in app

--- app.vue
+++ app.vue
@@ -4,66 +4,55 @@
   </NuxtLayout>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import '@unocss/reset/tailwind.css'
 
 import useHamburgerMenu from './composables/useHamburgerMenu'
 import useWindowSize from './composables/useWindowSize'
 
-export default {
-  setup() {
-    const { width } = useWindowSize() // Start listening to window size with the app's 'document reference'.
-    const { open, menuAvailable, toggle } = useHamburgerMenu(width)
+const { width } = useWindowSize() // Start listening to window size with the app's 'document reference'.
+const { toggle } = useHamburgerMenu(width)
 
-    const listenToEscapeKey = () => {
-      document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-          toggle(false)
-        }
-      })
+const listenToEscapeKey = () => {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      toggle(false)
     }
-
-    onMounted(() => {
-      listenToEscapeKey()
-    })
-
-    useHead({
-      title: 'Martijn Loth',
-      titleTemplate(title) {
-        return title ? `${title} - Martijn Loth` : 'Martijn Loth'
-      },
-      meta: [
-        { charset: 'utf-8' },
-        {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1',
-        },
-      ],
-      link: [
-        {
-          rel: 'icon',
-          type: 'image/png',
-          href: '/favicon/favicon.png',
-          media: 'prefers-color-scheme: light',
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          href: '/favicon/favicon-dark.png',
-          media: 'prefers-color-scheme: dark',
-        },
-      ],
-      htmlAttrs: {
-        lang: 'en',
-      },
-    })
-
-    return {
-      open,
-      menuAvailable,
-
-      toggle,
-    }
-  },
+  })
 }
+
+onMounted(() => {
+  listenToEscapeKey()
+})
+
+useHead({
+  title: 'Martijn Loth',
+  titleTemplate(title) {
+    return title ? `${title} - Martijn Loth` : 'Martijn Loth'
+  },
+  meta: [
+    { charset: 'utf-8' },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon/favicon.png',
+      media: 'prefers-color-scheme: light',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon/favicon-dark.png',
+      media: 'prefers-color-scheme: dark',
+    },
+  ],
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
 </script>
